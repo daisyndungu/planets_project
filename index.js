3b476fa97125bb56fe54a6e5d905ee3c84828648
daisyndungu <daisywndungu@gmail.com>
2022-07-11 03:50:20 +0300

    filter habitable planets

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const fs = require('fs');
 
 const results = [];
 
+const isHabitablePlanet = (planet) => {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+};
+
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
         comment: '#',
         columns: true // returns data as a KV pair instead of an array of items
     }))
     .on('data', (data) => {
-        results.push(data);
+        if(isHabitablePlanet(data)){
+            results.push(data);
+        }
     })
     .on('error', (err) => {
-        console.log('Err', err);
+        console.log('Opps! Something went wrong', err);
     })
     .on('end', () => {
-        console.log(results);
-        console.log('Done!!');
-    })
+        // print the habitable planet names
+        console.log(results.map(planet => {
+            return planet['kepler_name'];
+        }))
+        console.log(`We have ${results.length} habitable planets!!!`);
+    });
